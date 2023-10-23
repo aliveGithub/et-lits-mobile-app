ad6f90388b47948284d8007b4ab539423aa7a363
gmutuhu <geraldmutuhu@gmail.com>
2023-10-23 18:24:10 +0300

    AL-273 make registration atomic
    AL-273 make registration atomic

--- app/src/main/java/org/moa/etlits/data/repositories/AnimalRegistrationRepository.java
+++ app/src/main/java/org/moa/etlits/data/repositories/AnimalRegistrationRepository.java
@@ -1,7 +1,6 @@
 package org.moa.etlits.data.repositories;
 
 import android.app.Application;
-import android.util.Log;
 
 import org.moa.etlits.data.dao.AnimalDao;
 import org.moa.etlits.data.dao.AnimalRegistrationDao;
@@ -47,22 +46,7 @@ public class AnimalRegistrationRepository {
 
     public void insert(AnimalRegistration animalRegistration, List<Animal> animalList, List<Treatment> treatmentList) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
-            long animalRegId = animalRegistrationDao.insert(animalRegistration);
-            Log.d("AnimalRegRepo", "insert: " + animalRegId);
-            if (animalList != null) {
-                for (Animal animal : animalList) {
-                    animal.setAnimalRegistrationId(animalRegId);
-                    animalDao.insert(animal);
-                }
-            }
-
-            if (treatmentList != null){
-                for (Treatment treatment : treatmentList) {
-                    treatment.setAnimalRegistrationId(animalRegId);
-                    Log.d("AnimalRegRepo", "insert: " + treatment.getTreatmentApplied());
-                    treatmentDao.insert(treatment);
-                }
-            }
+            animalRegistrationDao.insertRegistration(animalRegistration, animalList, treatmentList, animalDao, treatmentDao);
         });
     }
 
@@ -71,75 +55,10 @@ public class AnimalRegistrationRepository {
             animalRegistrationDao.update(animalRegistration);
         });
     }
-
-    private boolean isInNewAnimalList(List<Animal> newList, long animalId) {
-        for (Animal animal : newList) {
-            if (animal.getId() == animalId) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isInNewTreatmentList(List<Treatment> newList, long treatmentId) {
-        for (Treatment treatment : newList) {
-            if (treatment.getId() == treatmentId) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void updateAnimals(long animalRegistrationId, List<Animal> newList) {
-        List<Animal> oldAnimalList = animalDao.getListByAnimalRegistrationId(animalRegistrationId);
-        if (oldAnimalList != null) {
-            for (Animal animal : oldAnimalList) {
-                if (!isInNewAnimalList(newList, animal.getId())) {
-                    animalDao.delete(animal.getId());
-                }
-            }
-        }
-
-        if (newList != null) {
-            for (Animal animal : newList) {
-                if (animal.getId() == 0) {
-                    animal.setAnimalRegistrationId(animalRegistrationId);
-                    animalDao.insert(animal);
-                } else {
-                    animalDao.update(animal);
-                }
-            }
-        }
-    }
-
-    private void updateTreatments(long animalRegistrationId, List<Treatment> newList) {
-        List<Treatment> oldTreatmentList = treatmentDao.getListByAnimalRegistrationId(animalRegistrationId);
-        if (oldTreatmentList != null) {
-            for (Treatment treatment : oldTreatmentList) {
-                if (!isInNewTreatmentList(newList, treatment.getId())) {
-                    treatmentDao.delete(treatment.getId());
-                }
-            }
-        }
-
-        if (newList != null) {
-            for (Treatment treatment : newList) {
-                if (treatment.getId() == 0) {
-                    treatment.setAnimalRegistrationId(animalRegistrationId);
-                    treatmentDao.insert(treatment);
-                } else {
-                    treatmentDao.update(treatment);
-                }
-            }
-        }
-    }
-
     public void update(AnimalRegistration animalRegistration,
                        List<Animal> animalList, List<Treatment> treatmentList) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
-            animalRegistrationDao.update(animalRegistration);
-            updateAnimals(animalRegistration.getId(), animalList);
-            updateTreatments(animalRegistration.getId(), treatmentList);
+            animalRegistrationDao.updateRegistration(animalRegistration, animalList, treatmentList, animalDao, treatmentDao);
         });
     }
 
