3329047a8bdbea905fe5a37c63a8c1b6d0fc1363
Ibrahim Mohammed <ibromoh.change@gmail.com>
2023-10-30 20:17:15 +0300

    fix: move 'terms of use' text to resources

--- app/src/main/java/org/moa/etlits/ui/activities/TermsOfUseActivity.java
+++ app/src/main/java/org/moa/etlits/ui/activities/TermsOfUseActivity.java
@@ -88,7 +88,7 @@ public class TermsOfUseActivity extends AppCompatActivity {
 
         if(actionBar != null) {
 
-            actionBar.setTitle("Terms of Use");
+            actionBar.setTitle(getString(R.string.ToU));
             actionBar.setDisplayHomeAsUpEnabled(true);
 
             final Drawable upArrow = ContextCompat.getDrawable(this, androidx.appcompat.R.drawable.abc_ic_ab_back_material);
